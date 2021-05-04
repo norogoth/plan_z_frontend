744d39f59ae22d40888fc2eb231ab06e35fafd3b
norogoth <norooth@gmail.com>
2021-05-04 09:07:54 -0700

    commiting all my stuff

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -7,25 +7,24 @@ export default function LoginPage() {
     return (
         <div>
 			<div class="contentDiv">
-            <h1>Aaker Budget</h1>
-            <ul>
-                <li>Set goals, enter transactions, and track progress</li>
-                <li>Use checkpoints to ensure your budget is accurate</li>
-            </ul>
-            <div className="loginDiv">
-                <div class="usernameDiv">
-                    <label>username:</label><input></input>
-                </div>
-                <div name="passwordDiv">
-                    <label>password:</label><input></input>
-                </div>
-                <div name="googleDiv">
-                    <label>Log in with your Google Account</label>
-                    <button onClick = {() => handleClick()} class="googleLoginButton">
-                        <img alt="Google login" class="googleLoginImage" src="google_login_1.png"/>
-                    </button>
-                </div>
-            </div>
+					<h1>My Budget</h1>
+					<ul>
+						<li class="feature-li">Set goals, enter transactions, and track progress</li>
+						<li class="feature-li">Use checkpoints to ensure your budget is accurate</li>
+					</ul>
+					<div className="loginDiv">
+						<div class="usernameDiv">
+							<label>username:</label><input></input>
+						</div>
+						<div class="passwordDiv">
+							<label>password:</label><input></input>
+						</div>
+						<div class="googleDiv">
+							<button onClick = {() => handleClick()} class="googleLoginButton">
+								<img alt="Google login" class="googleLoginImage" src="google_login_1.png"/>
+							</button>
+						</div>
+					</div>
 			</div>
         </div>
     );
