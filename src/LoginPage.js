08c9e00d71c6422eb91d75c9fd35dd97b99071a4
norogoth <norogoth@gmail.com>
2021-05-12 14:42:20 -0700

    more stuff jaja

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -1,11 +1,37 @@
+import { GoogleLogin, GoogleLogout } from 'react-google-login';
+
 import {Link} from 'react-router-dom';
+import { useState } from 'react';
+
+let clientId = process.env.REACT_APP_CLIENT_ID;
+let responseGoogle = null;
+
+console.log('Client ID: ',clientId);
 
 export default function LoginPage() {
+	const [loading, setLoading] = useState('Loading . . .');
+	const [user, setUser] = useState(null);
 
-    function handleClick() {
-		console.log('handle click called.');
-	}
+	const handleLogin = async googleData => {
+		
+		console.log('fetch to backend was called.');
 
+		const res = await fetch('http://localhost:8000/api/v1/auth/google', {
+			method: 'POST',
+			body: JSON.stringify({
+				token: googleData.tokenId
+			}),
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		})
+		console.log('res: ',res);
+		const data = await res.json()
+		responseGoogle = await data;
+
+		console.log('fetch to back reached EOF');
+	};
+		
     return (
         <div>
 			<div className="contentDiv">
@@ -23,11 +49,18 @@ export default function LoginPage() {
 					</div>
 					<button><Link to='/budget'>Log In</Link></button>
 				</div>
-				<div className="googleDiv">
-					<button onClick = {() => handleClick()} className="googleLoginButton">
+				<GoogleLogin
+    clientId={process.env.REACT_APP_CLIENT_ID}
+    buttonText="Log in with Google"
+    onSuccess={handleLogin}
+    onFailure={handleLogin}
+    cookiePolicy={'single_host_origin'}
+/><div className="googleDiv">
+			{/*
+					<button onClick = {() => handleLogin()} className="googleLoginButton">
 						<img alt="Google login" className="googleLoginImage" src="googleButton.png"/>
 					</button>
-					<p>Or sign in directly</p>
+			*/}
 				</div>
 			</div>
         </div>
